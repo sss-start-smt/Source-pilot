1490dead4362f795f01af16d104d308ae137d3c6
SSS-start <smt1027574097@163.com>
2026-08-28 12:26:39 +0800

    Delete config directory

--- config/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-"""SourcePilot configuration package."""
-from app.config.settings import PROJECT_ROOT, Settings, load_settings
-
-__all__ = ["Settings", "load_settings", "PROJECT_ROOT"]
